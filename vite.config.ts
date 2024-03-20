98a6c8a7be506fc3d70c405915ee4b834ffc3b65
cesardddp <cesardddp@hotmail.com>
2024-03-20 15:06:35 -0300

    fix instalável

---
+++ vite.config.ts
@@ -0,0 +1,73 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { resolve } from "path";
+
+
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      name: 'jsonforms-vue-primevue',
+      fileName: (format:any) => `jsonforms-vue-primevue.${format}.ts`,
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: [
+        'vue',
+        "lodash",
+        "lodash.debounce",
+        "cpf-cnpj-validator",
+        "@jsonforms/core",
+"@jsonforms/vue",
+"@vee-validate/i18n",
+"@vee-validate/rules",
+"@vueuse/core",
+"ajv",
+"core-js",
+"cpf-cnpj-validator",
+"dot-object",
+"lodash",
+"maska",
+"object-path",
+"password-validator",
+"primeicons",
+"primevue",
+"v-mask",
+"vee-validate",
+"vite-tsconfig-paths",
+"vue",
+"yup",
+      ],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+
+        globals:{
+
+          vue: 'Vue',
+          "lodash":"lodash",
+          "@jsonforms/core":"@jsonforms/core",
+          "@jsonforms/vue":"@jsonforms/vue",
+          "@vee-validate/i18n":"@vee-validate/i18n",
+          "@vee-validate/rules":"@vee-validate/rules",
+          "@vueuse/core":"@vueuse/core",
+          "ajv":"ajv",
+          "core-js":"core-js",
+          "cpf-cnpj-validator":"cpf-cnpj-validator",
+          "dot-object":"dot-object",
+          "maska":"maska",
+          "object-path":"object-path",
+          "password-validator":"password-validator",
+          "primeicons":"primeicons",
+          "primevue":"primevue",
+          "v-mask":"v-mask",
+          "vee-validate":"vee-validate",
+          "vite-tsconfig-paths":"vite-tsconfig-paths",
+          "yup":"yup",
+        }
+    }
+  }
+  }
+});
