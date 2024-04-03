02dce8716fee63693e8e761009cda3898a3f47ba
cesardddp <cesardddp@hotmail.com>
2024-04-03 09:45:05 -0300

    fix typescript build não gerado types

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,31 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { resolve } from "path";
+import dts from 'vite-plugin-dts'
 
+// example dts use with Vue https://github.com/qmhc/vite-plugin-dts/blob/main/examples/vue/vite.config.ts
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), dts(
+    {
+      // copyDtsFiles: true,
+      outDir: [
+        'dist',
+        // 'types'
+        // 'types/inner'
+      ],
+      // compilerOptions: {
+      //   declarationMap: true
+      // },
+    }
+  )],
   build: {
     minify: false,
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
       name: 'jsonforms-vue-primevue',
-      fileName: (format:any) => `jsonforms-vue-primevue.${format}.ts`,
+      fileName: (format: any) => `jsonforms-vue-primevue.${format}.ts`,
+      formats: ['es'],
     },
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
@@ -21,54 +36,54 @@ export default defineConfig({
         "lodash.debounce",
         "cpf-cnpj-validator",
         "@jsonforms/core",
-"@jsonforms/vue",
-"@vee-validate/i18n",
-"@vee-validate/rules",
-"@vueuse/core",
-"ajv",
-"core-js",
-"cpf-cnpj-validator",
-"dot-object",
-"lodash",
-"maska",
-"object-path",
-"password-validator",
-"primeicons",
-"primevue",
-"v-mask",
-"vee-validate",
-"vite-tsconfig-paths",
-"vue",
-"yup",
+        "@jsonforms/vue",
+        "@vee-validate/i18n",
+        "@vee-validate/rules",
+        "@vueuse/core",
+        "ajv",
+        "core-js",
+        "cpf-cnpj-validator",
+        "dot-object",
+        "lodash",
+        "maska",
+        "object-path",
+        "password-validator",
+        "primeicons",
+        "primevue",
+        "v-mask",
+        "vee-validate",
+        "vite-tsconfig-paths",
+        "vue",
+        "yup",
       ],
       output: {
+
         // Provide global variables to use in the UMD build
         // for externalized deps
-
-        globals:{
+        globals: {
 
           vue: 'Vue',
-          "lodash":"lodash",
-          "@jsonforms/core":"@jsonforms/core",
-          "@jsonforms/vue":"@jsonforms/vue",
-          "@vee-validate/i18n":"@vee-validate/i18n",
-          "@vee-validate/rules":"@vee-validate/rules",
-          "@vueuse/core":"@vueuse/core",
-          "ajv":"ajv",
-          "core-js":"core-js",
-          "cpf-cnpj-validator":"cpf-cnpj-validator",
-          "dot-object":"dot-object",
-          "maska":"maska",
-          "object-path":"object-path",
-          "password-validator":"password-validator",
-          "primeicons":"primeicons",
-          "primevue":"primevue",
-          "v-mask":"v-mask",
-          "vee-validate":"vee-validate",
-          "vite-tsconfig-paths":"vite-tsconfig-paths",
-          "yup":"yup",
+          "lodash": "lodash",
+          "@jsonforms/core": "@jsonforms/core",
+          "@jsonforms/vue": "@jsonforms/vue",
+          "@vee-validate/i18n": "@vee-validate/i18n",
+          "@vee-validate/rules": "@vee-validate/rules",
+          "@vueuse/core": "@vueuse/core",
+          "ajv": "ajv",
+          "core-js": "core-js",
+          "cpf-cnpj-validator": "cpf-cnpj-validator",
+          "dot-object": "dot-object",
+          "maska": "maska",
+          "object-path": "object-path",
+          "password-validator": "password-validator",
+          "primeicons": "primeicons",
+          "primevue": "primevue",
+          "v-mask": "v-mask",
+          "vee-validate": "vee-validate",
+          "vite-tsconfig-paths": "vite-tsconfig-paths",
+          "yup": "yup",
         }
+      }
     }
-  }
   }
 });
