e8cd3cad8aa76c5f8bd2d0a7e56f59a05c948ca7
cesardddp <cesardddp@hotmail.com>
2024-04-16 12:06:24 -0300

    add datetime field

---
+++ src/components/controladores/dateTime.vue
@@ -0,0 +1,53 @@
+<script lang="ts" setup>
+import type { ControlElement } from "@jsonforms/core";
+import { rendererProps, useJsonFormsControl } from "@jsonforms/vue";
+import wrapper from "./wrapper.vue";
+
+import Calendar from 'primevue/calendar';
+import { computed, ref } from "vue";
+
+const props = defineProps(rendererProps<ControlElement>());
+
+const controler = useJsonFormsControl(props);
+
+const wrapperRef = ref<InstanceType<typeof wrapper> | null>(null);
+
+const value = computed({
+	get: () => {
+		if (!wrapperRef.value?.value) {
+			return null;
+		}
+		const date = new Date(wrapperRef.value.value as string);
+		return date
+	},
+	set: (newValue) => {
+		// converter para o format YYYY-MM-DDT
+		// hh:mm:ss.sssZ
+		const dataFormatada = newValue?.toISOString();
+		dataFormatada && wrapperRef.value?.handleChange(dataFormatada);
+	}
+});
+
+</script>
+<template>
+	<wrapper ref="wrapperRef" :path="controler.control.value.path" :label="controler.control.value.label">
+		<!-- <pre>
+timeOnly: {{controler.control.value.uischema.options?.timeOnly}}
+showTime: {{controler.control.value.uischema.options?.showTime}}
+ampm: {{controler.control.value.uischema.options?.ampm}}
+dateTimeFormat: {{controler.control.value.uischema.options?.dateTimeFormat}}
+timeFormat: {{controler.control.value.uischema.options?.timeFormat}}
+	</pre> -->
+
+		<!-- date -->
+		<Calendar v-model="value" type="date" :id="controler.control.value.path" :name="controler.control.value.path"
+			:time-only="controler.control.value.uischema.options?.timeOnly ?? true"
+			:show-time="controler.control.value.uischema.options?.showTime ?? false"
+			:hourFormat="controler.control.value.uischema.options?.ampm ? '12' : '24'" :date-format="controler.control.value.uischema.options?.dateTimeFormat ?? 'dd/mm/yy' +
+		controler.control.value.uischema.options?.timeFormat ?? 'hh:mm:ss'
+		"
+		append-to="body"
+		/>
+
+	</wrapper>
+</template>
